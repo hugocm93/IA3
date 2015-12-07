926d3e69e08a7a4f73971cfb922030eb14d1272a
Hugo Costa Machado <hugocm93@gmail.com>
2015-12-07 12:11:04 -0200

    Saving

--- ArffGenerator/src/main.java
+++ ArffGenerator/src/main.java
@@ -1,28 +1,14 @@
-import java.io.DataInputStream;
-import java.io.IOException;
 
 public class main {
 
 	public static void main(String[] args) {
-		/* Reader - Reads a character from the console. Makes sure the user read the information that was printed*/
-		DataInputStream in=new DataInputStream(System.in);
-
 
 		/* The attribute generator class generates a map of strings and integers. The string corresponds to a word, and the integer corresponds
-		 * to the number of times that the word appears in all the files. It also creates the listOfWords.txt file. This file needs to
-		 * be manually filtered before generating the arff file.
+		 * to the number of times that the word appears in all the files. It also creates the listOfWords.txt file.
 		 */
 		System.out.println("The list of attributes is going to be generated.");
 		AttributeGenerator att = new AttributeGenerator();
-		System.out.println("The list of attributes was generated.\nPlease, filter the list manually in order to continue with the creation of the arff file.\nThen, press any key to continue.");
-
-
-		/* Reader - Reads a character from the console. Makes sure the user read the information that was printed*/
-		try {
-			byte b = in.readByte();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		System.out.println("The list of attributes was generated.");
 
 		
 		/*The arff generator class receives the map that was created on the step above. It is currently not using the map, but it could use
