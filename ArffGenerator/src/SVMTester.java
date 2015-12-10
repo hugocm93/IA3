cd92cdc3cef455b6f08deea1db872b8b38e0db1a
Hugo <hugocm93@gmail.com>
2015-12-10 01:59:50 -0200

    Saving

--- ArffGenerator/src/SVMTester.java
+++ ArffGenerator/src/SVMTester.java
@@ -4,6 +4,15 @@ import java.io.IOException;
 
 
 public class SVMTester {
+	/*Paths and names of files*/
+	String userDir = System.getProperty("user.dir");
+	String arff = new String("/MovieDataSet_REDUCED.arff");
+
+	int generalCounter = 0;
+	String allReviews[][] = new String[50000][];
+
+	int rightAnswer = 0;
+	int wrongAnswer = 0;
 
 	//ler arquivo arff
 
@@ -24,16 +33,13 @@ public class SVMTester {
 	////////////////////Início
 
 	public SVMTester() {
-		/*Paths and names of files*/
-		String userDir = System.getProperty("user.dir");
-		String arff = new String("/MovieDataSet_REDUCED.arff");
 
 		/*Reads the arff file*/
 		try (BufferedReader br = new BufferedReader(new FileReader(userDir + arff))) {
-			
+
 			/*Counts each attribute of the file*/
 			int i=0;
-			
+
 			/*There are 51 attributes, but let's add some extra space*/
 			String[] attributes = new String[60];
 
@@ -43,7 +49,7 @@ public class SVMTester {
 
 			/*While to capture each word and save it to a string. I am not sure if this is going to be needed, but anyway...*/
 			while(i<51){
-				
+
 				/*Last line has a different pattern*/
 				if(i==50){
 					String aux = br.readLine();
@@ -54,8 +60,8 @@ public class SVMTester {
 					System.out.println(aux);
 					attributes[i++] = aux;
 				}
-				
-				
+
+
 				/*Read each line and extract the word*/
 				String aux = br.readLine();
 				aux = aux.replaceAll("@attribute ", "");
@@ -64,44 +70,78 @@ public class SVMTester {
 				System.out.println(aux);
 				attributes[i++] = aux;
 			}
-			
+
 			/*Reads garbage*/
 			br.readLine();
-			
-			
+
+
 			/*Variable to iterate over the reviews*/
 			int revs = 0;
-			
+
 			/*for each review*/
 			while(revs<50000){
-				
+
 				/*Reads the line*/
 				String aux2 = br.readLine();
-				
+
 				/*Split the numbers by , or space*/
 				String[] reviews = aux2.split(",| ");
+				allReviews[revs] = reviews;
 
 				/*Prints all the numbers of a review in a line.*/
-				for(int j=0;j<51;j++){
-					System.out.print(reviews[j] + " ");
-					if(j==50){
-						System.out.println();
-					}
-				}
-				
-				
+				//				for(int j=0;j<51;j++){
+				//					System.out.print(reviews[j] + " ");
+				//					if(j==50){
+				//						System.out.println();
+				//					}
+				//				}
+
+
 				//Here you can pass some of the information to the SVM algorith for testing purposes. Let's say 66%
 				//SVM_function(String[] reviews);
-				
+
 				/*Go to next review*/
 				revs++;
 			}
-
 
 		}
 		catch(IOException e){
 			System.out.println("File not found");
 			e.printStackTrace();
 		}
+
+
+		/*Now that the model was built, I can test it with all instances*/
+
+
+		/*Prints Everything*/
+		for(int i=0;i<50000;i++){
+			for(int k=0;k<51;k++){
+				System.out.print(allReviews[i][k] + " ");
+				if(k==50){
+					System.out.println();
+				}
+			}
+		}
+
+		for(int indexOfReview=0 ; indexOfReview<50000 ; indexOfReview++){
+			int received = SVM_applyOnModel(allReviews[indexOfReview]);
+			int expected = Integer.parseInt(allReviews[indexOfReview][50]);
+			if(received==expected){
+				rightAnswer++;
+			}
+			else{
+				wrongAnswer++;
+			}
+		}
+
+		System.out.println("Right answers: " + (rightAnswer/50000.)*100 + "%");
+		System.out.println("Wrong answers: " + (wrongAnswer/50000.)*100 + "%");
+	}
+	
+	int SVM_applyOnModel(String review[]){
+		
+		
+		return 0;
 	}
 }
